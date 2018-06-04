a0bf5681b02ff8867df4d3a285d11e0af02bc37c
Raj Maitra <raj.d.maitra@gmail.com>
2018-06-04 16:03:10 -0700

    update

---
+++ week1/test_scripts/file.py
@@ -0,0 +1,7 @@
+def reverse_string(string):
+    return string[::-1]
+
+a = "Soony is looney"
+
+b = reverse_string(a)
+print b
---
+++ week1/test_scripts/test.py
@@ -0,0 +1,14 @@
+array = [123, 395844, 29038472, 48794]
+
+# for loop 
+sum = 0 
+for i in array:
+    sum = sum + i
+    print sum
+
+
+a = range(0,3)
+
+print a
+
+
